f7b2cd71a99574e91a659f70f3e15a626ad10f80
Raúl Arellano <raul338@elhacker.net>
2020-03-22 21:49:21 -0300

    Add Crud action & listeners tests

--- tests/src/TestController.php
+++ tests/src/TestController.php
@@ -3,10 +3,63 @@ namespace Raul338\Phpstan\Tests;
 
 use Cake\Controller\Controller;
 
+/**
+ * @property \Crud\Controller\Component\CrudComponent $Crud
+ */
 class TestController extends Controller
 {
-    public function index()
+    public function loadModelTest()
     {
         $this->loadModel('Test');
+    }
+
+    /**
+     * Crud Action Tests
+     */
+    public function add()
+    {
+        $this->loadComponent('Crud');
+        $this->Crud->action()->saveOptions([]);
+    }
+
+    public function edit()
+    {
+        $this->loadComponent('Crud');
+        $this->Crud->action()->saveOptions([]);
+    }
+
+    public function delete()
+    {
+        $this->loadComponent('Crud');
+        $this->Crud->action()->findMethod([]);
+    }
+
+    public function index()
+    {
+        $this->loadComponent('Crud');
+        $this->Crud->action()->findMethod('');
+    }
+
+    public function view()
+    {
+        $this->loadComponent('Crud');
+        $this->Crud->action()->findMethod('');
+    }
+
+    public function customActionTest()
+    {
+        $this->loadComponent('Crud');
+        $this->Crud->mapAction('customActionTest', 'Crud.Index');
+        /** @var \Crud\Action\IndexAction $action */
+        $action = $this->Crud->action();
+        $action->findMethod('');
+    }
+
+    public function relatedModelListenerTest()
+    {
+        $this->loadComponent('Crud');
+        $this->Crud->mapAction('relatedModelListenerTest', 'Crud.Index');
+        $this->Crud->addListener('relatedModels', 'Crud.RelatedModels');
+        $this->Crud->listener('relatedModels')->relatedModels(true);
     }
 }
