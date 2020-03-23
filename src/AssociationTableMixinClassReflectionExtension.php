5d8b7920a07b7e9ec2315e3dbae90be85d01ee2d
Raúl Arellano <raul338@elhacker.net>
2020-03-23 16:39:44 -0300

    Fix FindBy code

--- src/AssociationTableMixinClassReflectionExtension.php
+++ src/AssociationTableMixinClassReflectionExtension.php
@@ -4,52 +4,22 @@ declare(strict_types=1);
 namespace Raul338\Phpstan\Cake;
 
 use Cake\ORM\Table;
-use PHPStan\Broker\Broker;
-use PHPStan\Reflection\BrokerAwareExtension;
 use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\MethodReflection;
 use PHPStan\Reflection\MethodsClassReflectionExtension;
 
-class AssociationTableMixinClassReflectionExtension implements
-    MethodsClassReflectionExtension,
-    BrokerAwareExtension
+class AssociationTableMixinClassReflectionExtension implements MethodsClassReflectionExtension
 {
-    /**
-     * @var \PHPStan\Broker\Broker
-     */
-    private $broker;
-
-    public function setBroker(Broker $broker): void
-    {
-        $this->broker = $broker;
-    }
-
-    protected function getTableReflection(): ClassReflection
-    {
-        return $this->broker->getClass(Table::class);
-    }
-
     public function hasMethod(ClassReflection $classReflection, string $methodName): bool
     {
-        // magic findBy* method
-        if ($classReflection->isSubclassOf(Table::class)) {
-            if (preg_match('/^find(?:\w+)?By/', $methodName) > 0) {
-                return true;
-            }
-        }
+        $isTable = $classReflection->isSubclassOf(Table::class);
+        $isFindBy = preg_match('/^find(?:\w+)?By/', $methodName) > 0;
 
-        return $this->getTableReflection()->hasNativeMethod($methodName);
+        return $isTable && $isFindBy;
     }
 
     public function getMethod(ClassReflection $classReflection, string $methodName): MethodReflection
     {
-        // magic findBy* method
-        if ($classReflection->isSubclassOf(Table::class)) {
-            if (preg_match('/^find(?:\w+)?By/', $methodName) > 0) {
-                return new TableFindByPropertyMethodReflection($methodName, $classReflection);
-            }
-        }
-
-        return $this->getTableReflection()->getNativeMethod($methodName);
+        return new TableFindByPropertyMethodReflection($methodName, $classReflection);
     }
 }
